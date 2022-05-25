d1b14a835f55bd66cb3d8c3b8466170cd0f18153
justinkirk8 <96632211+justinkirk8@users.noreply.github.com>
2022-05-25 15:45:15 -0400

    13.5.6 update

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -1,7 +1,7 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// We create the tile layer that will be the background of our map.
+//We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -38,8 +38,8 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [30, 30],
-  zoom: 2,
+  center: [43.7, -79.3],
+  zoom: 11,
   layers: [streets]
 })
 
@@ -48,39 +48,61 @@ L.control.layers(baseMaps).addTo(map);
 
 
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/justinkirk8/Mapping_Earthquakes/main/majorAirports.json";
+// // Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/justinkirk8/Mapping_Earthquakes/main/majorAirports.json";
 
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h2>Airport Code: " + layer.feature.properties.faa + "</h2> <hr> <h4>Airport name: " + layer.feature.properties.name + "</h4");
-  }}).addTo(map);
-});
+// // Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJSON(data, {
+//   onEachFeature: function(feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h2>Airport Code: " + layer.feature.properties.faa + "</h2> <hr> <h4>Airport name: " + layer.feature.properties.name + "</h4");
+//   }}).addTo(map);
+// });
 
 
-// Create a style for the lines.
+// // Create a style for the lines.
+// let myStyle = {
+//   color: "#ffffa1",
+//   weight: 2
+// }
+
+
+// // Accessing the Toronto airline routes GeoJSON URL.
+// let torontoData = "https://raw.githubusercontent.com/justinkirk8/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// // Grabbing our GeoJSON data.
+// d3.json(torontoData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJSON(data, {
+//   style: myStyle,
+//   onEachFeature: function(feature, layer) {
+//     layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
+//   }
+// }).addTo(map);
+// });
+
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/justinkirk8/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// // Create a style for the lines.
 let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+  fillColor: "yellow",
+  weight: 1
 }
 
-
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/justinkirk8/Mapping_Earthquakes/main/torontoRoutes.json";
-
-// Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+//Grabbing out GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  style: myStyle,
-  onEachFeature: function(feature, layer) {
-    layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
-  }
-}).addTo(map);
-});
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+      style: myStyle,
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("<h3> Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+      }
+  }).addTo(map);
+})
